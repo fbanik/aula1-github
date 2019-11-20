610495d0aad5a98d74bb3ae32417fcf454cd9747
Fernando Banik <fernandobanik@gmail.com>
2019-11-20 13:25:59 -0300

    Mensagem inicial

--- src/application/aprendendoGIT.java
+++ src/application/aprendendoGIT.java
@@ -3,7 +3,7 @@ package application;
 public class aprendendoGIT {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Aprendendo como trabalhar com git/gitHub!!");
 
 	}
 
